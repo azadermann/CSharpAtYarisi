a5ba6499ef438e3dd7b3debffe47ca174a87eef8
azadermann <azaderman7@gmail.com>
2022-11-08 01:05:35 +0300

    Proje dosyası ekle.

---
+++ AtYarisi/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace AtYarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BittisLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.YazıcıLabel = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-1, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1258, 26);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-1, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1258, 26);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(-1, 288);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1258, 26);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(-1, 437);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1258, 26);
+            this.label4.TabIndex = 3;
+            // 
+            // BittisLabel
+            // 
+            this.BittisLabel.BackColor = System.Drawing.Color.Black;
+            this.BittisLabel.Location = new System.Drawing.Point(1217, 24);
+            this.BittisLabel.Name = "BittisLabel";
+            this.BittisLabel.Size = new System.Drawing.Size(40, 413);
+            this.BittisLabel.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button1.Location = new System.Drawing.Point(515, 470);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 76);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "BAŞLA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // YazıcıLabel
+            // 
+            this.YazıcıLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.YazıcıLabel.ForeColor = System.Drawing.Color.Yellow;
+            this.YazıcıLabel.Location = new System.Drawing.Point(12, 480);
+            this.YazıcıLabel.Name = "YazıcıLabel";
+            this.YazıcıLabel.Size = new System.Drawing.Size(1233, 51);
+            this.YazıcıLabel.TabIndex = 9;
+            this.YazıcıLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::AtYarisi.Properties.Resources.GleefulScarceBushsqueaker_size_restricted;
+            this.pictureBox3.Location = new System.Drawing.Point(2, 317);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(201, 106);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::AtYarisi.Properties.Resources.AT_YARIŞI;
+            this.pictureBox2.Location = new System.Drawing.Point(2, 165);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(201, 106);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AtYarisi.Properties.Resources.SnoopyGreenBubblefish_max_1mb;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1257, 558);
+            this.Controls.Add(this.YazıcıLabel);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BittisLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "At Yarışı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label BittisLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label YazıcıLabel;
+    }
+}
+
---
+++ AtYarisi/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtYarisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int birinciatınsolauzaklık, ikinciatsolauzaklık, ucuncuatsolauzaklık;
+        Random rnd = new Random();
+       private void SesOynatma()
+        {
+        SoundPlayer sndply = new SoundPlayer(@"C:\\Users\\azade\\OneDrive\\Masaüstü\\res\\atsesi.wav");
+            sndply.Play();
+            
+        }
+        private void SesDurdurma()
+        {
+            SoundPlayer sndply = new SoundPlayer(@"C:\\Users\\azade\\OneDrive\\Masaüstü\\res\\atsesi.wav");
+            sndply.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SesOynatma();
+            YazıcıLabel.Visible = true;
+            button1.Visible = false;
+            if (timer1.Enabled == false)
+            {
+                timer1.Start();
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Left > pictureBox2.Left && pictureBox1.Left > pictureBox3.Left)
+            {
+                YazıcıLabel.Text = "Bir Numaralı At Yarışı Önde Götürüyor Değerli Seyirciler ";
+            }
+            else if (pictureBox2.Left > pictureBox1.Left && pictureBox2.Left > pictureBox3.Left)
+            {
+                YazıcıLabel.Text = "İki Numaralı Yarışı Önde Götürüyor Değerli Seyirciler ";
+            }
+            else if (pictureBox3.Left > pictureBox1.Left && pictureBox3.Left > pictureBox2.Left)
+            {
+                YazıcıLabel.Text = "Üç Numaralı At Yarışı Önde Götürüyor Değerli Seyirciler ";
+            }
+            int birinciatgenislik = pictureBox1.Width;
+            int ikinciatgenislik = pictureBox2.Width;
+            int ucuncuatgenislik = pictureBox3.Width;
+            int bittiscizgisi = BittisLabel.Left;
+
+            pictureBox1.Left += rnd.Next(2, 17);
+            pictureBox2.Left += rnd.Next(2, 15);
+            pictureBox3.Left += rnd.Next(2, 15);
+
+            if (pictureBox1.Left + pictureBox1.Width > bittiscizgisi)
+            {
+                timer1.Stop();
+                SesDurdurma();
+                MessageBox.Show("Bir Numaralı At Yarışı Kazandı");
+                pictureBox1.Left = birinciatınsolauzaklık;
+                pictureBox2.Left = ikinciatsolauzaklık;
+                pictureBox3.Left = ucuncuatsolauzaklık;
+                YazıcıLabel.Text = null;
+                button1.Visible = true;
+                YazıcıLabel.Visible = false;
+
+            }
+            else if (pictureBox2.Left + pictureBox2.Width > bittiscizgisi)
+            {
+                timer1.Stop();
+                SesDurdurma();
+                MessageBox.Show("İki Numaralı At Yarışı Kazandı");
+                pictureBox1.Left = birinciatınsolauzaklık;
+                pictureBox2.Left = ikinciatsolauzaklık;
+                pictureBox3.Left = ucuncuatsolauzaklık;
+                YazıcıLabel.Text = null;
+                button1.Visible = true;
+                YazıcıLabel.Visible = false;
+
+            }
+            else if (pictureBox3.Left + pictureBox3.Width > bittiscizgisi)
+            {
+                timer1.Stop();
+                SesDurdurma();
+                MessageBox.Show("Üç Numaralı At Yarışı Kazandı");
+                pictureBox1.Left = birinciatınsolauzaklık;
+                pictureBox2.Left = ikinciatsolauzaklık;
+                pictureBox3.Left = ucuncuatsolauzaklık;
+                YazıcıLabel.Text = null;
+                button1.Visible = true;
+                YazıcıLabel.Visible = false;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            YazıcıLabel.Visible = false;
+            birinciatınsolauzaklık = pictureBox1.Left;
+            ikinciatsolauzaklık = pictureBox2.Left;
+            ucuncuatsolauzaklık = pictureBox3.Left;
+
+
+        }
+    }
+}
